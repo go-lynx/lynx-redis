0b5f11e80f2a5a8f5875b6ad20c5f9a40c4841b2
tan-zhuo <tanzhuo@aliyun.com>
2026-03-08 23:23:00 +0900

    chore: bump pluginVersion to v1.5.5

--- plugin_meta.go
+++ plugin_meta.go
@@ -9,7 +9,7 @@ const (
 	// Plugin unique name
 	pluginName = "redis.client"
 	// Plugin version number
-	pluginVersion = "v1.5.4"
+	pluginVersion = "v1.5.5"
 	// Plugin description
 	pluginDescription = "redis plugin for lynx framework"
 	// Configuration prefix, used to read plugin-related configuration from config
